44ec6993643822bff4891a4d3d9dbfc59607fb6a
chetan0541 <33390795+chetan0541@users.noreply.github.com>
2020-01-22 20:52:23 +0100

    Update CNN-dog-cat.py

--- CNN-dog-cat.py
+++ CNN-dog-cat.py
@@ -2,13 +2,16 @@ import cv2
 import tensorflow as tf 
 
 categories = ["Dog", "Cat"]
-path = 'C:/Users/Chetan Tuli/Desktop/Dogs and cats samples/kitten.png'
+path = 'C:/Users/Chetan Tuli/Desktop/Samples/cat.png'
 
 def prepare(filepath):
     img_size = 150
-    img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
-    new_array = cv2.resize(img_array, (img_size, img_size))
-    return new_array.reshape(-1, img_size, img_size, 1)
+    try:
+        img_array = cv2.imread(filepath, cv2.IMREAD_GRAYSCALE)
+        new_array = cv2.resize(img_array, (img_size, img_size))
+        return new_array.reshape(-1, img_size, img_size, 1)
+    except Exception as e:
+        print(str(e))
 
 model = tf.keras.models.load_model("64x3-CNN.model")
 
